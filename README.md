13556cf1b31871ba77881593d74e99c492695895
sokevinjonas <kjonasdevpro@gmail.com>
2025-03-01 00:02:17 +0000

    installation & configurations du projet

---
+++ README.md
@@ -0,0 +1,12 @@
+## Portfolio Développeur Web & Mobile
+
+Portfolio personnel présentant mes projets et compétences en développement web et mobile.
+
+### Technologies
+
+Laravel 12
+Laravel Cloud (déploiement)
+
+### Contact
+
+SO Kevin Jonas Gningnabe - kjonasdevpro@gmail.com
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!--=============== FONT AWESOME ===============-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
+
+    <!--=============== CSS ===============-->
+    <link rel="stylesheet" href="{{  asset('assets/css/styles.css')}}" />
+
+    <title>Responsive portfolio website - Crypticalcoder</title>
+  </head>
+  <body>
+    <!--==================== STYLE SWITCHER ====================-->
+    <div class="style__switcher"></div>
+
+    <!--==================== HEADER ====================-->
+    <header class="header"></header>
+
+    <!--==================== MAIN ====================-->
+    <main class="main">
+      <!--==================== HOME ====================-->
+      <section class="home" id="home"></section>
+
+      <!--==================== ABOUT ====================-->
+      <section class="about" id="about"></section>
+
+      <!--==================== QUALIFICATION ====================-->
+      <section class="qualification" id="qualification"></section>
+
+      <!--==================== SERVICES ====================-->
+      <section class="services" id="services"></section>
+
+      <!--==================== SKILLS ====================-->
+      <section class="skills"></section>
+
+      <!--==================== PORTFOLIO ====================-->
+      <section class="works" id="works"></section>
+
+      <!--==================== TESTIMONIALS ====================-->
+      <section class="testimonials"></section>
+
+      <!--==================== CONTACT ====================-->
+      <section class="contact" id="contact"></section>
+    </main>
+
+    <!--==================== FOOTER ====================-->
+    <footer class="footer"></footer>
+  </body>
+</html>
